f1c6a7db25489145ba4015677646ca6ca9ea1072
berrehal ouala eddine <berrehaloualaeddine@gmail.com>
2019-09-18 14:20:30 +0100

    working normalement

--- src/relaiKtalyse.js
+++ src/relaiKtalyse.js
@@ -2,7 +2,7 @@ const Configuration = require('./Configuration.js');
 const BeaconScanner = require('./BLEScanner.js');
 const MQTTClient = require('./MQTTClient.js');
 console.log("init configuration");
-configuration = new Configuration("../configuration.json");
+configuration = new Configuration("./configuration.json");
 beaconScanner = new BeaconScanner();
 mqttClient = new MQTTClient(configuration);
 beaconScanner.addObserver(mqttClient);
